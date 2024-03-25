30554b868bf221f588f4ed893388c2b354bb0dad
Jacek Abramczyk <ja@tricent.com>
2024-03-25 16:06:40 +0100

    Add ScoreBoardManager implementation

--- Sportradar/Sportradar.Library/IScoreBoardManager.cs
+++ Sportradar/Sportradar.Library/IScoreBoardManager.cs
@@ -9,3 +9,26 @@ public interface IScoreBoardManager
     Task FinishMatchAsync(int matchId);
     Task<List<FootballMatch>> GetMatchesAsync();
 }
+
+public class ScoreBoardManager : IScoreBoardManager
+{
+    public Task<FootballMatch> StartNewMatchAsync(string homeTeamName, string awayTeamName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<FootballMatch> UpdateScoreAsync(int matchId, int homeTeamScore, int awayTeamScore)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task FinishMatchAsync(int matchId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<FootballMatch>> GetMatchesAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
