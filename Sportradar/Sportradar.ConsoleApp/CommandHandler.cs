e09543aef3f4ca4f618f4c5758b35c487da0b1b6
Jacek Abramczyk <ja@tricent.com>
2024-03-26 13:21:52 +0100

    Add error handler

--- Sportradar/Sportradar.ConsoleApp/CommandHandler.cs
+++ Sportradar/Sportradar.ConsoleApp/CommandHandler.cs
@@ -6,27 +6,50 @@ public class CommandHandler
 {
     public static void FinishMatchCommand(string[] command, IScoreBoardManager scoreBoardManager)
     {
-        var homeTeamName = command[2];
-        var awayTeamName = command[4];
+        try
+        {
+            var homeTeamName = command[2];
+            var awayTeamName = command[4];
                     
-        scoreBoardManager.FinishMatchAsync(homeTeamName, awayTeamName).Wait();
+            scoreBoardManager.FinishMatchAsync(homeTeamName, awayTeamName).Wait();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Invalid command for finish match, exception: {e.Message}");
+        }
+        
     }
 
     public static void UpdateMatchCommand(string[] command, IScoreBoardManager scoreBoardManager)
     {
-        var homeTeamName = command[2];
-        var homeTeamScore = int.Parse(command[3]);
-        var awayTeamName = command[5];
-        var awayTeamScore = int.Parse(command[6]);
+        try
+        {
+            var homeTeamName = command[2];
+            var homeTeamScore = int.Parse(command[3]);
+            var awayTeamName = command[5];
+            var awayTeamScore = int.Parse(command[6]);
                     
-        scoreBoardManager.UpdateScoreAsync(homeTeamName, homeTeamScore, awayTeamName, awayTeamScore).Wait();
+            scoreBoardManager.UpdateScoreAsync(homeTeamName, homeTeamScore, awayTeamName, awayTeamScore).Wait();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Invalid command for update match, exception: {e.Message}");
+        }
+        
     }
 
     public static void StartMatchCommand(string[] command, IScoreBoardManager scoreBoardManager)
     {
-        var homeTeamName = command[2];
-        var awayTeamName = command[4];
-                    
-        scoreBoardManager.StartNewMatchAsync(homeTeamName, awayTeamName).Wait();
+        try
+        {
+            var homeTeamName = command[2];
+            var awayTeamName = command[4];
+
+            scoreBoardManager.StartNewMatchAsync(homeTeamName, awayTeamName).Wait();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Invalid command for start match, exception: {e.Message}");
+        }
     }
 }
