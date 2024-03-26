67c9516cc796709097cb6fe6b067477c350d058e
Jacek Abramczyk <ja@tricent.com>
2024-03-26 12:51:04 +0100

    Add unit tests

--- Sportradar/Sportradar.Library.Tests/ScoreBoardManagerTests.cs
+++ Sportradar/Sportradar.Library.Tests/ScoreBoardManagerTests.cs
@@ -1,15 +1,73 @@
+using Moq;
+using Sportradar.Library.Models;
+using Sportradar.Library.Repositories;
+
 namespace Sportradar.Library.Tests;
 
+[TestFixture]
 public class ScoreBoardManagerTests
 {
+    private ScoreBoardManager _sut;
+    private Mock<ITeamRepository> _teamRepository;
+    private Mock<IFootballMatchRepository> _footballMatchRepository;
+
     [SetUp]
     public void Setup()
     {
+        _teamRepository = new Mock<ITeamRepository>();
+        _footballMatchRepository = new Mock<IFootballMatchRepository>();
+        
+        
+        _sut = new ScoreBoardManager(_teamRepository.Object, _footballMatchRepository.Object);
     }
 
     [Test]
-    public void Test1()
+    public void StartNewMatchAsync_ForNotExistingTeams_ShouldCreateThem()
     {
-        Assert.Pass();
+        //arrange
+        var notExistingHomeTeamName = "notExistingHomeTeam";
+        var notExistingAwayTeamName = "notExistingAwayTeam";
+        
+        _teamRepository.Setup(x => x.GetTeamAsync(It.IsAny<string>())).ReturnsAsync((Team)null!);
+
+        var createdHomeTeam = new Team(notExistingHomeTeamName);
+        var createdAwayTeam = new Team(notExistingAwayTeamName);
+        
+        _teamRepository.Setup(x => x.CreateTeamAsync(notExistingHomeTeamName)).ReturnsAsync(createdHomeTeam);
+        _teamRepository.Setup(x => x.CreateTeamAsync(notExistingAwayTeamName)).ReturnsAsync(createdAwayTeam);
+        
+        //act
+        var result = _sut.StartNewMatchAsync(notExistingHomeTeamName, notExistingAwayTeamName).Result;
+        
+        //assert
+        _teamRepository.Verify(x => x.GetTeamAsync(notExistingHomeTeamName), Times.Once);
+        _teamRepository.Verify(x => x.GetTeamAsync(notExistingAwayTeamName), Times.Once);
+        
+        _teamRepository.Verify(x => x.CreateTeamAsync(notExistingHomeTeamName), Times.Once);
+        _teamRepository.Verify(x => x.CreateTeamAsync(notExistingAwayTeamName), Times.Once);
     }
+
+    [Test]
+    public void StartNewMatchAsync_ForExistingTeams_ShouldReturnThem()
+    {
+        var homeTeamName = "HomeTeam";
+        var awayTeamName = "AwayTeam";
+        
+        var homeTeam = new Team(homeTeamName);
+        var awayTeam = new Team(awayTeamName);
+        
+        _teamRepository.Setup(x => x.GetTeamAsync(homeTeamName)).ReturnsAsync(homeTeam);
+        _teamRepository.Setup(x => x.GetTeamAsync(awayTeamName)).ReturnsAsync(awayTeam);
+        
+        //act
+        var result = _sut.StartNewMatchAsync(homeTeamName, awayTeamName).Result;
+        
+        //assert
+        _teamRepository.Verify(x => x.GetTeamAsync(homeTeamName), Times.Once);
+        _teamRepository.Verify(x => x.GetTeamAsync(awayTeamName), Times.Once);
+        
+        _teamRepository.Verify(x => x.CreateTeamAsync(It.IsAny<string>()), Times.Never);
+    }
+    
+    
 }
